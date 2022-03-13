ec85b50994b1eb856957732a26c2932a48d0593b
Adam Koch <me@adamko.ch>
2022-03-13 15:36:39 -0400

    Added Brobeans' Ecstasy.

--- Sheeesh.lua
+++ Sheeesh.lua
@@ -1,3 +1,4 @@
 -- Sheesh!
 local LSM = LibStub("LibSharedMedia-3.0")
 LSM:Register("sound", "Sheeesh", [[Interface\AddOns\Sheeesh\Media\sheeesh.mp3]])
+LSM:Register("sound", "Brobeans Ecstasy", [[Interface\AddOns\Sheeesh\Media\Brobeans.ogg]])
