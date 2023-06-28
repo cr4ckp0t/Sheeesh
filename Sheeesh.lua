36defa43d68714f6779b66dee2ff10d9c4fc53cd
Adam Koch <me@adamko.ch>
2023-06-27 22:15:55 -0400

    Added Phay raid soak calls.

--- Sheeesh.lua
+++ Sheeesh.lua
@@ -2,3 +2,7 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 LSM:Register("sound", "Sheeesh", [[Interface\AddOns\Sheeesh\Media\sheeesh.mp3]])
 LSM:Register("sound", "Brobeans Ecstasy", [[Interface\AddOns\Sheeesh\Media\Brobeans.ogg]])
+LSM:Register("sound", "Phay - Group 1 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group1-soak.mp3]])
+LSM:Register("sound", "Phay - Group 2 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group2-soak.mp3]])
+LSM:Register("sound", "Phay - Group 3 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group3-soak.mp3]])
+LSM:Register("sound", "Phay - Group 4 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group4-soak.mp3]])
