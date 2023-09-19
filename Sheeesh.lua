5d4b6808f15f0c7360b2d4a4263cf97ab0daacb5
Adam Koch <me@adamko.ch>
2023-09-18 22:35:44 -0400

    Added new sound.

--- Sheeesh.lua
+++ Sheeesh.lua
@@ -6,3 +6,4 @@ LSM:Register("sound", "Phay - Group 1 Soak", [[Interface\AddOns\Sheeesh\Media\ph
 LSM:Register("sound", "Phay - Group 2 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group2-soak.mp3]])
 LSM:Register("sound", "Phay - Group 3 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group3-soak.mp3]])
 LSM:Register("sound", "Phay - Group 4 Soak", [[Interface\AddOns\Sheeesh\Media\phay-group4-soak.mp3]])
+LSM:Register("sound", "Phay - Step on Me", [[Interface\AddOns\Sheeesh\Media\phay_step_on_me.mp3]])
